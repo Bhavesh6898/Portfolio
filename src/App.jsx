e2bc0ac58319f1d265d8661f558cc2c72e7b64f8
Bhavesh Dutt Myla <bhaveshduttmyla@Bhaveshs-MacBook-Pro.local>
2025-05-10 19:36:24 -0500

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Experiences from './sections/Experiences';
 import Testimonial from './sections/Testimonial';
 import Contact from './sections/Contact';
 import Footer from './sections/Footer';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
